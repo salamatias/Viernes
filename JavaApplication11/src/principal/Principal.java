837ae99ee613ec16367e44bac5e9081e91d043c1
salamatias <salamancamatias@gmail.com>
2016-09-23 20:50:44 -0300

    agrega 2

--- JavaApplication11/src/principal/Principal.java
+++ JavaApplication11/src/principal/Principal.java
@@ -12,7 +12,8 @@ public class Principal
      public static void main(String[] args)
      {
          System.out.println("Bienvenido");
+         System.out.println("Adios");
      }
 }
      
-}
+
